66212d590ba8d5e009819acb73e6c2212e4c13dd
ngocphuongthaole-hub <ngocphuongthao.le@students.mq.edu.au>
2026-05-23 09:02:23 +1000

    import firebase, get html element, login fucntion, get email and password entered by user

---
+++ js/auth.js
@@ -0,0 +1,33 @@
+// Import Firebase Authentication
+import { auth } from "firebase'js";
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js"
+
+
+// Get HTML elements
+const emailInput = document.getElementById("emailInput");
+const passwordInput = document.getElementById("passwordInput");
+const signInButton = document.getElementById("signInButton");
+const messageArea = document.getElementById("messageArea");
+
+// Run login function
+// when user clicks sign in button
+signInButton.addEventListener("click",...)
+
+// get email and password entered by user
+emailInput.value 
+passwordInput.value
+
+// try login with Firebase
+await signInWithEmailAndWord(auth, emailInput, password);
+
+// if login successful
+
+// show success message
+// redirect to home page
+
+
+// if login fails
+// show error message
