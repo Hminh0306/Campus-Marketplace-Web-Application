42536bcbe49228686148d1eaa4f20b938109d772
ngocphuongthaole-hub <ngocphuongthao.le@students.mq.edu.au>
2026-05-23 10:31:39 +1000

    Display current user email on index.html

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// Import auth from firebase.js
+import { auth } from "./firebase.js";
+
+// Import auth state listener from Firebase Auth
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+
+// Get HTML element for displaying current user email
+const currentUserEmail =
+  document.getElementById("currentUserEmail");
+
+
+// Listen for authentication state
+onAuthStateChanged(auth, function (user) {
+
+  // If user is logged in
+  if (user) {
+
+    // Display current user email on index.html
+    currentUserEmail.textContent =
+      "Logged in as: " + user.email;
+
+  } else {
+
+    // If user is not logged in, redirect to login page
+    window.location.href = "login.html";
+  }
+});
